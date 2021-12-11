1f5b224252da74475a59d497e354346c52bbcb6a
catharinez <91547640+catharinez@users.noreply.github.com>
2021-12-11 15:22:01 -0500

    Add files via upload

---
+++ python project.py
@@ -0,0 +1,62 @@
+#*************************************************
+
+# Author: Catharine and Noshin
+
+# Date: December 11, 2021
+
+# Last modified: December 11, 2021 3:12 pm by Catharine
+
+# Name: Python Project (Wildlife Survival Guide)
+
+# Description: Pygame project which teaches a lesson about wildlife survival
+
+#*************************************************
+
+# Import modules
+import pygame
+pygame.init() # Initialize pygame
+import platform
+import os, time
+
+# Necessary for python to work on tdsb computers
+if platform.system() == "Windows":
+    os.environ['SDL_VIDEODRIVER'] = 'windib'
+
+# Define functions
+def gameLoop():
+    x = 0
+    y = 0
+    gameExit = False
+    
+    while not gameExit:
+        for event in pygame.event.get():
+            if event.type == pygame.quit:
+                pygame.quit()
+                quit()
+         
+        gameDisplay.fill((0, 0, 0)) #set background as green 
+        homeScreen(x, y)
+            
+        pygame.display.update()
+        
+        clock.tick(60)
+        
+def homeScreen(x, y):        
+    gameDisplay.blit(homeScreenBg, (x, y))
+            
+# Main program
+# Declare and initialize variables
+displayWidth = int(800)
+displayHeight = int(600)
+gameExit = bool()
+homeScreenBg = pygame.image.load('WildlifeSurvivalHomeScreen.png')
+icon = pygame.image.load('icon.png')
+
+# Display screen
+gameDisplay = pygame.display.set_mode((displayWidth, displayHeight))
+pygame.display.set_caption('Wildlife Survival Guide') # Sets caption at top to Wildlife Survival Guide
+pygame.display.set_icon(icon) # Sets icon
+
+clock = pygame.time.Clock()
+
+gameLoop()
